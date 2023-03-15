4615494e3b0c48b1bba76fcfb006600c7cc7b178
husterL9 <2670227240@qq.com>
2023-03-15 10:24:31 +0800

    merge

--- 剑指offer/09用两个栈实现队列.js
+++ 剑指offer/09用两个栈实现队列.js
@@ -1,15 +1,30 @@
-var CQueue = function () {}
+var CQueue = function () {
+  this.TPoint = 0
+  this.Hstack = []
+  this.Tstack = []
+}
 
 /**
  * @param {number} value
  * @return {void}
  */
-CQueue.prototype.appendTail = function (value) {}
+CQueue.prototype.appendTail = function (value) {
+  this.Tstack.push(value)
+  this.TPoint = this.Tstack.length
+}
 
 /**
  * @return {number}
  */
-CQueue.prototype.deleteHead = function () {}
+CQueue.prototype.deleteHead = function () {
+  if (this.Hstack.length === 0) {
+    this.Tstack.reverse()
+    this.TPoint = 0
+    this.Tstack.forEach((item) => {
+      this.Hstack.push(item)
+    })
+  }
+}
 
 /**
  * Your CQueue object will be instantiated and called as such:
