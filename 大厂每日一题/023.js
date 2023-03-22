add59ffbbbfb45844d3c31aa2ea2bc4cec694eef
husterL9 <2670227240@qq.com>
2023-03-23 00:35:10 +0800

    feat(023)

--- 大厂每日一题/023.js
+++ 大厂每日一题/023.js
@@ -1,2 +1,17 @@
-let str = '𝒳😂'
-console.log(str.length, str[0], str[1])
+//https://jishuin.proginn.com/p/763bfbd67a9b
+async function target() {
+  let res = await fetch('https://www.baidu.com')
+  console.log('do something')
+  let res1 = await fetch('https://www.baidu.com')
+  return res
+}
+let res = target().then((res) => {
+  console.log(res)
+})
+function* gen() {}
+function immitate(generator) {
+  return function () {
+    return new Promise((resolve, reject) => {})
+  }
+}
+let asyncFn = immitate(gen)
