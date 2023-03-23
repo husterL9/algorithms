5e571156a572cdd210161f78a42578d8d31d45b5
husterL9 <2670227240@qq.com>
2023-03-23 09:40:50 +0800

    feat(23)

--- 大厂每日一题/023.js
+++ 大厂每日一题/023.js
@@ -1,2 +1,23 @@
-let str = '𝒳😂'
-console.log(str.length, str[0], str[1])
+//https://jishuin.proginn.com/p/763bfbd67a9b
+//https://fairyly.github.io/interview/3.3.1%20Async---Await%E5%8E%9F%E7%90%86.html#%E7%AE%80%E4%BB%8B
+function* generateSequence(start, end) {
+  for (let i = start; i <= end; i++) yield i
+}
+
+function* generatePasswordCodes() {
+  // 0..9
+  yield* generateSequence(48, 57)
+
+  // A..Z
+  yield* generateSequence(65, 90)
+
+  // a..z
+  yield* generateSequence(97, 122)
+}
+
+let str = ''
+
+for (let code of generatePasswordCodes()) {
+  console.log(code)
+  str += String.fromCharCode(code)
+}
