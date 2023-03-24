bf5326ba7c9381c176baec3675698a3c2dd969dc
husterL9 <2670227240@qq.com>
2023-03-24 19:19:56 +0800

    fix

--- 大厂每日一题/023.js
+++ 大厂每日一题/023.js
@@ -1,17 +1,39 @@
 //https://jishuin.proginn.com/p/763bfbd67a9b
-async function target() {
-  let res = await fetch('https://www.baidu.com')
-  console.log('do something')
-  let res1 = await fetch('https://www.baidu.com')
-  return res
+// async function target() {
+//   let res = await fetch('https://www.baidu.com')
+//   console.log('do something')
+//   let res1 = await fetch('https://www.baidu.com')
+//   return res
+// }
+// let res = target().then((res) => {
+//   console.log(res)
+// })
+// function asyncOperator(num) {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(num)
+//     }, 2000)
+//   })
+// }
+function* gen(num) {
+  let num1 = yield asyncOperator(num)
+  let num2 = yield asyncOperator(num1)
+  let num3 = yield asyncOperator(num2)
+  return num3
 }
-let res = target().then((res) => {
-  console.log(res)
-})
-function* gen() {}
 function immitate(generator) {
   return function () {
     return new Promise((resolve, reject) => {})
   }
 }
-let asyncFn = immitate(gen)
+
+function autoGen(generator) {
+  let gen = generator()
+
+  return function () {
+    return new Promise((resolve) => {
+      go(resolve)
+    })
+  }
+}
+let asyncFn = autoGen(gen)
