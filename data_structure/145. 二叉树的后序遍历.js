fc5a45ae0975200dea55890a77d0b4074f7f5074
husterL9 <2670227240@qq.com>
2023-03-29 12:32:35 +0800

    feat(145)

--- data_structure/145. 二叉树的后序遍历.js
+++ data_structure/145. 二叉树的后序遍历.js
@@ -25,4 +25,28 @@ var postorderTraversal = function (root) {
   // post(root)
 
   //迭代
+  if (!root) {
+    return res
+  }
+  //[1,null,2,3]
+  stack.push(root)
+  while (root && stack.length > 0) {
+    let leftNode = root.left
+    let rightNode = root.right
+    while (leftNode) {
+      stack.push(leftNode)
+      leftNode = leftNode.left
+      if (leftNode) {
+        rightNode = leftNode.right
+      }
+    }
+
+    if (rightNode) {
+      root = rightNode
+    } else {
+      let node = stack.pop()
+      res.push(node.val)
+    }
+  }
+  return res
 }
